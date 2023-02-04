7c81a9f1f32d9e9b301701ed874374bbb8629c37
Arshdeep724 <singharshdeep46@gmail.com>
2023-02-05 02:02:21 +0530

    finished middleware

--- src/auth/logged-check.middleware.ts
+++ src/auth/logged-check.middleware.ts
@@ -1,10 +1,20 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { UserService } from 'src/user/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
 export class SecurityCheck implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    if (req.headers['x-secret-key'] === process.env.SECRET_KEY) {
+  async use(req: Request, res: Response, next: NextFunction) {
+    if(req.body.token === undefined || req.body.email === undefined) {
+      return res.status(400).send("Token undefined");
+    }
+    const prismaService = new PrismaService();
+    const userService = new UserService(prismaService);
+    const user: CreateUserDto = await userService.findByEmailId(req.body.email);
+    
+    if (user.token !== req.body.token) {
       res.status(401).send({
         error: 'Unauthorized! User Not Logged In',
       });
